96662a161c6bafc669e309df718a41a3e777fea4
LudovicVRDN <vourdon@live.fr>
2026-04-21 17:44:39 +0200

    Feat : Navbar design done

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,17 +1,22 @@
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { Route, Routes } from "react-router"
+import Navbar from "./components/Navbar"
 
  function App() {
 
 
   return (
     <>
+    <Navbar />
      <Routes>
+      
       <Route path="/" element></Route>
       <Route path="register" element></Route>
       <Route path="profile" element></Route>
       <Route path="profile/edit" element></Route>
 
      </Routes>
+      <ReactQueryDevtools />
     </>
   )
 }
